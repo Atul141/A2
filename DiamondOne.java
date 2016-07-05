8427933e12b82a57c80d3e3693b6c4025bcc91b0
Atul Kumar <atulbk@thoughtworks.com>
2016-07-05 23:45:46 +0530

    completed

--- DiamondOne.java
+++ DiamondOne.java
@@ -7,17 +7,23 @@ public class DiamondOne {
 
     public static void main(String[] args) {
 
-        int num=Integer.parseInt(args[0]);
-        int i,j;
-        for (i = 0; i < num;i++)
-        {
-            for(j=0;j<=(num-i-1);j++)
+        int num = Integer.parseInt(args[0]);
+        isosceles(num);
+
+    }
+
+    public static void isosceles(int num) {
+        int i, j;
+
+        for (i = 0; i < num; i++) {
+            for (j = 0; j <= (num - i - 1); j++)
                 System.out.print(" ");
 
-            for(j=0;j<=(i*2);j++)
+            for (j = 0; j <= (i * 2); j++)
                 System.out.print("*");
 
             System.out.println();
         }
+
     }
 }
---
+++ DiamondTwo.java
@@ -0,0 +1,28 @@
+package A2;
+
+/**
+ * Created by Jayashree on 05-07-2016.
+ */
+public class DiamondTwo {
+
+    public static void main(String[] args)
+    {
+        int num=Integer.parseInt(args[0]);
+        DiamondOne.isosceles(num);
+        centered(num-1);
+    }
+    public static void centered(int num)
+    {
+        int i,j;
+        for(i=num;i>0;i--)
+        {
+            for(j=num;j>=i-1;j--)
+                System.out.print(" ");
+
+            for(j=0;j<(i*2-1);j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
