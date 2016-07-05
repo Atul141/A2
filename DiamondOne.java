e7a1e967a598ca2238a151e4ecf2abd997717fb1
Atul Kumar <atulbk@thoughtworks.com>
2016-07-05 23:13:25 +0530

    completed

--- DiamondOne.java
+++ DiamondOne.java
@@ -6,15 +6,17 @@ package A2;
 public class DiamondOne {
 
     public static void main(String[] args) {
-int num=20;
-        //int num=Integer.parseInt(args[0]);
+
+        int num=Integer.parseInt(args[0]);
         int i,j;
         for (i = 0; i < num;i++)
         {
-            for(j=0;j<=num-i-1;j++)
+            for(j=0;j<=(num-i-1);j++)
                 System.out.print(" ");
+
             for(j=0;j<=(i*2);j++)
-            System.out.print("*");
+                System.out.print("*");
+
             System.out.println();
         }
     }
